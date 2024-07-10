a2e065350a83995db924192e29f623925cff1c0c
faheemfazal <faheemfazalpkv@gmail.com>
2024-07-10 19:12:20 +0530

    url issueee

--- src/axiosLink/axios.js
+++ src/axiosLink/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // Create an instance of axios
 const axiosuser = axios.create({
-  baseURL: "https://sia-backend-eight.vercel.app/",
+  baseURL: "http://sia-backend-eight.vercel.app",
+ 
   headers: {
     "Content-Type": "application/json",
   },
