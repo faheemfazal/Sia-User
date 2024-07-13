79d55709371e3d73359debcf57a2214eedea2c67
faheemfazal <faheemfazalpkv@gmail.com>
2024-07-13 17:57:45 +0530

    add logo

--- src/Screens/WelcomeScreen.js
+++ src/Screens/WelcomeScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSpring, animated } from "react-spring";
 import { useNavigate } from "react-router-dom";
-// import img from '../assets/images/m Logo.png'
+import img from '../assets/images/logoSpon.png'; // Import your image here
 
 import LocationSelect from "../components/Modal/LocationSelect";
 
@@ -9,6 +9,9 @@ export default function WelcomeScreen() {
   const [ring1Style, setRing1Style] = useSpring(() => ({ padding: "0px" }));
   const [ring2Style, setRing2Style] = useSpring(() => ({ padding: "0px" }));
   const [textStyle, setTextStyle] = useSpring(() => ({ fontSize: "1vh" }));
+  const [imageStyle, setImageStyle] = useSpring(() => ({
+    transform: "translateY(100%)",
+  }));
   const [open, setOpen] = useState();
   const navigate = useNavigate(false);
 
@@ -22,8 +25,11 @@ export default function WelcomeScreen() {
     setTimeout(() => {
       setTextStyle({ fontSize: "5.2vh" }); // Animate text size from small to large
     }, 700);
+    setTimeout(() => {
+      setImageStyle({ transform: "translateY(0%)" }); // Animate image from bottom to position
+    }, 900);
     setTimeout(() => navigate("/home"), 2500);
-  }, [setRing1Style, setRing2Style, setTextStyle, navigate]);
+  }, [setRing1Style, setRing2Style, setTextStyle, setImageStyle, navigate]);
 
   return (
     <div className="flex flex-col justify-center items-center h-screen  bg-[#63247d] space-y-10">
@@ -56,6 +62,12 @@ export default function WelcomeScreen() {
         >
           Sia is always right
         </p>
+        <animated.img
+          src={img}
+          alt="Logo"
+          style={imageStyle}
+          className="w-20 h-20 mt-5" // Adjust the size and spacing as needed
+        />
       </div>
     </div>
   );
