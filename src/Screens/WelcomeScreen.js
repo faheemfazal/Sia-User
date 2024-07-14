3c3630dc765e6198665e6d0920ba950d0e3881b0
faheemfazal <faheemfazalpkv@gmail.com>
2024-07-14 11:50:24 +0530

    image

--- src/Screens/WelcomeScreen.js
+++ src/Screens/WelcomeScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSpring, animated } from "react-spring";
 import { useNavigate } from "react-router-dom";
-import img from '../assets/images/logoSpon.png'; // Import your image here
+import img from '../assets/images/logoSpon.jpg'; // Import your image here
 
 import LocationSelect from "../components/Modal/LocationSelect";
 
@@ -11,6 +11,7 @@ export default function WelcomeScreen() {
   const [textStyle, setTextStyle] = useSpring(() => ({ fontSize: "1vh" }));
   const [imageStyle, setImageStyle] = useSpring(() => ({
     transform: "translateY(100%)",
+    opacity: 0, // Initial state to hide the image
   }));
   const [open, setOpen] = useState();
   const navigate = useNavigate(false);
@@ -21,52 +22,40 @@ export default function WelcomeScreen() {
     }, 250);
     setTimeout(() => {
       setRing2Style({ padding: "3.8vh" }); // Assuming hp(5) is roughly 5vh
-    }, 450);
+    }, 350);
     setTimeout(() => {
       setTextStyle({ fontSize: "5.2vh" }); // Animate text size from small to large
-    }, 650);
+    }, 550);
     setTimeout(() => {
-      setImageStyle({ transform: "translateY(0%)" }); // Animate image from bottom to position
-    }, 900);
+      setImageStyle({ transform: "translateY(0%)", opacity: 1 }); // Animate image from bottom to position and make it visible
+    }, 800);
     setTimeout(() => navigate("/home"), 5500);
   }, [setRing1Style, setRing2Style, setTextStyle, setImageStyle, navigate]);
 
   return (
-    <div className="flex flex-col justify-center items-center h-screen  bg-[#63247d] space-y-10">
-      <animated.div
-        className="bg-white bg-opacity-20 rounded-full mx-2"
-        style={ring2Style}
-      >
-        <animated.div
-          className="bg-white bg-opacity-20 rounded-full"
-          style={ring1Style}
-        >
-          <animated.div
-            style={textStyle}
-            className="text-white font-bold tracking-widest"
-          >
+    <div className="flex flex-col justify-center items-center h-screen bg-[#63247d] space-y-10">
+      <animated.div className="bg-white bg-opacity-20 rounded-full mx-2" style={ring2Style}>
+        <animated.div className="bg-white bg-opacity-20 rounded-full" style={ring1Style}>
+          <animated.div style={textStyle} className="text-white font-bold tracking-widest">
             Pickupiko
           </animated.div>
         </animated.div>
       </animated.div>
       <div className="flex flex-col items-center space-y-2">
-        <h1
-          className="text-white font-bold tracking-widest"
-          style={{ fontSize: "4vh" }}
-        >
+        <h1 className="text-white font-bold tracking-widest" style={{ fontSize: "4vh" }}>
           SIA
         </h1>
-        <p
-          className="text-white font-medium tracking-widest"
-          style={{ fontSize: "2vh" }}
-        >
+        <p className="text-white font-medium tracking-widest" style={{ fontSize: "2vh" }}>
           Sia is always right
+        </p>
+        <p className="text-white font-medium tracking-widest" style={{ fontSize: "2vh" }}>
+          Sponsored by
         </p>
         <animated.img
           src={img}
           alt="Logo"
           style={imageStyle}
-          className="w-72 h-32 mt-5" // Adjust the size and spacing as needed
+          className="w-64 h-28" // Adjust the size and spacing as needed
         />
       </div>
     </div>
