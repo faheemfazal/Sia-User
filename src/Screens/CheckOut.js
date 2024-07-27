bfbadf28f7a7739ecc6dbda7da0dafe4ddd1907a
faheemfazal <faheemfazalpkv@gmail.com>
2024-07-27 16:08:12 +0530

    scanner

--- src/Screens/CheckOut.js
+++ src/Screens/CheckOut.js
@@ -417,7 +417,7 @@ export default function Checkout() {
                   <div className="text-center pt-6">
                   <button
         type="submit"
-        className="rounded-full px-4 py-2 mb-4 flex gap-2 text-[#63247d] hover:text-white hover:bg-[#63247d] font-bold text-xl border-2 border-[#63247d] whitespace-nowrap"
+        className="rounded-full px-4 py-2 mb-4 flex gap-2  items-center justify-center text-[#63247d] hover:text-white hover:bg-[#63247d] font-bold text-xl border-2 border-[#63247d] whitespace-nowrap"
         onClick={handleSubmit}
         style={{ height: '60px', width: '200px' }} // Fixed height and width for the button
         disabled={loading} // Disable the button when loading
