15b7a431e0d71560168a065c4d6498f52658bc23
faheemfazal <faheemfazalpkv@gmail.com>
2024-07-27 15:58:19 +0530

    scanner

--- src/Screens/CheckOut.js
+++ src/Screens/CheckOut.js
@@ -108,6 +108,7 @@ export default function Checkout() {
   }
 
   const handlePaymentChange = (event) => {
+   
     const { value } = event.target;
     setSelectedPayment(value);
   };
@@ -216,7 +217,7 @@ export default function Checkout() {
       if(response.status==201){
         Toast.show({
           icon: 'success',
-          content: 'Checkout successful',
+          content: response.data.message,
         });
         return navigate('/cart')
       }
@@ -416,7 +417,7 @@ export default function Checkout() {
                   <div className="text-center pt-6">
                   <button
         type="submit"
-        className="py-3 px-6 uppercase rounded-md w-full text-[#63247d] hover:text-white hover:bg-[#63247d] border-[#63247d] font-bold text-2xl border-2 flex items-center justify-center"
+        className="rounded-full px-4 py-2 mb-4 flex gap-2 text-[#63247d] hover:text-white hover:bg-[#63247d] font-bold text-xl border-2 border-[#63247d] whitespace-nowrap"
         onClick={handleSubmit}
         style={{ height: '60px', width: '200px' }} // Fixed height and width for the button
         disabled={loading} // Disable the button when loading
