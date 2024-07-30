d0b395cb200957443c72cab393eceb0d3c0a3a82
faheemfazal <faheemfazalpkv@gmail.com>
2024-07-30 11:21:33 +0530

    kjkj

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,25 +1,29 @@
 import { Header, Hero } from "../components";
-import {Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
-import {Autoplay, EffectFade} from 'swiper/modules';
+import { Autoplay, EffectFade } from 'swiper/modules';
 import { data } from "../data/data";
 
 const Banner = () => {
   return (
-   <Swiper
-    spaceBetween={30}
-    speed={3000}
-    height={30}
-    autoplay={{delay: 6000, disableOnInteraction: false}}
-    effect={"fade"}
-    fadeEffect={{crossFade: true}}
-    modules={[Autoplay, EffectFade]}
-    className="mySwiper"
-   >
-      {data.map(({id, colorDeep, colorLite, mainText, subText, shadow, mobileShadow, img}) => (
-        <SwiperSlide key={id} style={{backgroundColor: `${colorLite}`}} className="w-full h-36 flex flex-col md:gap-10 gap-4 pt-4 md:pt-8">
-          <Header colorDeep={colorDeep}/>
+    <Swiper
+      spaceBetween={30}
+      speed={3000}
+      height={30}
+      autoplay={{ delay: 6000, disableOnInteraction: false }}
+      effect={"fade"}
+      fadeEffect={{ crossFade: true }}
+      modules={[Autoplay, EffectFade]}
+      className="mySwiper h-36 md:h-64" // Add height classes here
+    >
+      {data.map(({ id, colorDeep, colorLite, mainText, subText, shadow, mobileShadow, img }) => (
+        <SwiperSlide 
+          key={id} 
+          style={{ backgroundColor: `${colorLite}` }} 
+          className="w-full h-full flex flex-col md:gap-10 gap-4 pt-4 md:pt-8"
+        >
+          <Header colorDeep={colorDeep} />
           <Hero 
             colorDeep={colorDeep}
             mainText={mainText}
@@ -30,8 +34,8 @@ const Banner = () => {
           />
         </SwiperSlide>
       ))}
-   </Swiper>
+    </Swiper>
   )
 }
 
-export default Banner
+export default Banner;
